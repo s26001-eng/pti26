6ba0d1fc4f97b919acc325fb5b37c926663da27a
itc-s26001 <s26001@std.it-college.ac.jp>
2026-07-06 16:49:10 +0900

    if_kadai.py追加

---
+++ if_kadai.py
@@ -0,0 +1,12 @@
+score = 80
+
+if score >= 90:
+    print("S")
+elif score >= 80:
+    print("A")
+elif score >= 70:
+    print("B")
+elif score >= 60:
+    print("C")
+else:
+    print("D（不可）")
---
+++ var9.py
@@ -0,0 +1,5 @@
+b = "こんにちは"
+if b.isdigit():       # bが数字（True）なら
+    print(int(b)+23)  # この計算を行う
+else:                 # Falseなら
+    print("数値じゃないよ") # エラーメッセージを出力する
